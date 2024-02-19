1ade062a2db05af691b7ee3480e26732d0b153b5
Pluvio <pluvio72@gmx.com>
2024-02-19 11:30:21 +0000

    added framer-motion, animation on hover controls
    Signed-off-by: Pluvio <pluvio72@gmx.com>

--- src/components/PlayerControls/PlayerControls.tsx
+++ src/components/PlayerControls/PlayerControls.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import BottomControls from './BottomControls';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import BottomControls from "./BottomControls";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div<{ info: DOMRect }>`
-  height: ${props => props.info.height}px;
-  width: ${props => props.info.width}px;
-  left: ${props => props.info.left}px;
+  height: ${(props) => props.info.height}px;
+  width: ${(props) => props.info.width}px;
+  left: ${(props) => props.info.left}px;
   flex-direction: column;
   align-items: center;
   position: absolute;
@@ -28,65 +29,91 @@ const TopControls = styled.div`
 `;
 
 export default function PlayerControls({ videoEl }: Props) {
-  const [playing, setPlaying] = useState(!videoEl.paused || false)
-  const [currentTime, setCurrentTime] = useState(videoEl.currentTime || 0)
+  const [playing, setPlaying] = useState(!videoEl.paused || false);
+  const [currentTime, setCurrentTime] = useState(videoEl.currentTime || 0);
+  const [mouseActive, setMouseActive] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (playing) {
-        setCurrentTime(videoEl.currentTime)
+        setCurrentTime(videoEl.currentTime);
       }
-    }, 1000)
+    }, 1000);
 
-    return () => clearInterval(interval)
-  }, [playing])
+    return () => clearInterval(interval);
+  }, [playing]);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    const handleMouseMove = () => {
+      setMouseActive(true);
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => setMouseActive(false), 2000);
+    }
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      clearTimeout(timeoutId);
+    }
+  }, []);
 
   const onPlay = () => {
-    videoEl.play()
-    setPlaying(true)
-  }
+    videoEl.play();
+    setPlaying(true);
+  };
 
   const onPause = () => {
-    videoEl.pause()
-    setPlaying(false)
-  }
+    videoEl.pause();
+    setPlaying(false);
+  };
 
   const togglePlaying = () => {
     if (playing) {
-      onPause()
+      onPause();
     } else {
-      onPlay() 
+      onPlay();
     }
-  }
+  };
 
   const seek = (value: number) => {
-    videoEl.fastSeek(value)
-    setCurrentTime(value)
-  }
+    videoEl.fastSeek(value);
+    setCurrentTime(value);
+  };
 
   const changeVolume = (newVolume: number) => {
     videoEl.volume = newVolume;
-  }
+  };
 
   return (
-    <Wrapper info={videoEl.getBoundingClientRect()}>
-      <TopControls onClick={togglePlaying}>
-        {playing ?
-          <Icon src={'images/pause-icon.png'} onClick={onPause} /> :
-          <Icon src={'images/play-icon.png'} onClick={onPlay} /> 
-        }
-      </TopControls>
-      <BottomControls progressBarInfo={{
-          progress: currentTime,
-          duration: videoEl.duration,
-          seekTo: seek
-        }}
-        changeVolume={changeVolume}
-      />
-    </Wrapper>
-  )
+    <motion.div
+      animate={{ opacity: mouseActive ? 1 : 0 }}
+      style={{ opacity: 0 }}
+      // onMouseMove={animateControls}
+    >
+      <Wrapper info={videoEl.getBoundingClientRect()} className="playerControls">
+        <TopControls onClick={togglePlaying}>
+          {playing ? (
+            <Icon src={"images/pause-icon.png"} onClick={onPause} />
+          ) : (
+            <Icon src={"images/play-icon.png"} onClick={onPlay} />
+          )}
+        </TopControls>
+        <BottomControls
+          progressBarInfo={{
+            progress: currentTime,
+            duration: videoEl.duration,
+            seekTo: seek,
+          }}
+          changeVolume={changeVolume}
+        />
+      </Wrapper>
+    </motion.div>
+  );
 }
 
 interface Props {
-  videoEl: HTMLVideoElement
+  videoEl: HTMLVideoElement;
 }
